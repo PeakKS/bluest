2e18b5392226411d5a4638947f5a54f8208930b2
PeakKS <bbatson101@gmail.com>
2024-04-22 13:38:43 -0400

    wrap address from_str with deviceid from_str

--- src/bluer.rs
+++ src/bluer.rs
@@ -7,7 +7,19 @@ pub mod service;
 
 mod error;
 
+use std::str::FromStr;
+
 /// A platform-specific device identifier.
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct DeviceId(bluer::Address);
+
+impl FromStr for DeviceId {
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match bluer::Address::from_str(s) {
+            Ok(addr) => Ok(DeviceId(addr)),
+            Err(e) => Err(e)
+        }
+    }
+    type Err = bluer::InvalidAddress;
+}
